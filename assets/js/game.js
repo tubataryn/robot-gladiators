a421b31de2029f33107d3a4cb9f0a80e85abb786
Taryn Fanning <taryn.fanning@gmail.com>
2020-06-17 13:21:56 -0500

    for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,11 @@ var playerMoney = 10
 
 console.log(playerName, playerAttack, playerHealth, playerMoney);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     //Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -68,4 +68,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
